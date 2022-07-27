e21ec8f314f53fcbc1eaab46cc3be16c38542c37
Jon Wehner <jonjwehner@gmail.com>
2022-07-26 17:51:51 -0700

    redis config

--- slightlylesssimple-web/index.js
+++ slightlylesssimple-web/index.js
@@ -2,14 +2,25 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  legacyMode: true,
+  socket: {
+    port: 6379,
+    host: 'redis-server',
+  },
+});
+client.connect();
 client.set('visits', 0);
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
+client.on('error', (err) => {
+  console.log('Error ' + err);
+});
+
+app.get('/', async (req, res) => {
+  await client.get('visits', (err, visits) => {
     res.send(`Number of visits is ${visits}`);
+    client.set('visits', parseInt(visits) + 1);
   });
-  client.set('visits', parseInt(visits) + 1);
 });
 
 app.listen(8081, () => {
